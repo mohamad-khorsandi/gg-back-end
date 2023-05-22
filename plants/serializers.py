cad50ad4b4bc6a2d37b9bbe054f6b3693ead483e
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-22 14:43:27 +0330

    Adding the plant model serializer

---
+++ plants/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from models import Plants
+
+
+class PlantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plants
+        fields = '__all__'
