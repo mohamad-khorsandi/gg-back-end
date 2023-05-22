ea1e05fb916bc4767b58bc189cdba8a3f403790a
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-22 14:52:46 +0330

    Adding the api address of the explore page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'home'
+app_name = 'home' # what is this?
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'), # what is this again?
+    path('home_plants_api', views.ExploreAPI.as_view(), name='explore_api'),
 ]
