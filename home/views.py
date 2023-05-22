ad0f726971aa0c5ab303eb755e4e4af236281e16
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-22 14:43:43 +0330

    adding explore api

--- home/views.py
+++ home/views.py
@@ -1,2 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions, status
+from ..plants.models import Plants
+from ..plants.serializers import PlantsSerializer
 
+
+class ExploreAPI(APIView):
+
+    def get(self, request):
+        query = Plants.objects.all()
+        serializers = PlantsSerializer(query, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
 
