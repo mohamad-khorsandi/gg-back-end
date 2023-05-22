d36ea3f71ef187ec135382c2603f396ba9e2a919
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-22 16:08:36 +0330

    Changing the APIView to ListAPIView

--- home/views.py
+++ home/views.py
@@ -1,15 +1,14 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import permissions, status
-from ..plants.models import Plants
-from ..plants.serializers import PlantsSerializer
+from plants.models import Plants
+from plants.serializers import PlantsSerializer
 
 
-class ExploreAPI(APIView):
-
-    def get(self, request):
-        query = Plants.objects.all()
-        serializers = PlantsSerializer(query, many=True)
-        return Response(serializers.data, status=status.HTTP_200_OK)
+class ExploreAPI(ListAPIView):
+    serializer_class = PlantsSerializer
+    permission_classes = [AllowAny]
+    queryset = Plants.objects.all()
 
