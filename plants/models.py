a926d8fa1c3fea085fd730276391bc3eb738cd83
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-26 11:52:16 +0330

    Editing the plant model

--- plants/models.py
+++ plants/models.py
@@ -1,27 +1,73 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 class Plant(models.Model):
+    LIGHT_CHOICES = [
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High'),
+    ]
+    TEMPERATURE_CHOICES = [
+        (1, 'Warm'),
+        (2, 'Sultry'),
+    ]
+    WATER_CHOICES = [
+        (1, 'Everyday'),
+        (2, 'Each two day'),
+        (3, 'Once a week'),
+        (4, 'Each two weeks'),
+    ]
+    PLANT_CHOICES = [
+        (1, 'Prickly'),
+        (2, 'With Flower'),
+        (3, 'Flowerless'),
+    ]
+    LOCATION_TYPE_CHOICES = [
+        (1, 'Apartment'),
+        (2, 'Close'),
+        (3, 'Open'),
+    ]
+    GROWTH_CHOICES = [
+        (1, 'Seed'),
+        (2, 'Sapling'),
+        (3, 'Complete'),
+    ]
+    ATTENTION_NEED_CHOICES = [
+        (1, 'Everyday'),
+        (2, 'Weekly'),
+        (3, 'Monthly'),
+    ]
+    SEASON_CHOICES = [
+        (1, 'Spring'),
+        (2, 'Summer'),
+        (3, 'Fall'),
+        (4, 'Winter'),
+    ]
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
+    maintenance = models.TextField(null=True, blank=True)
     fragrance = models.BooleanField()
     type = models.CharField(max_length=50)
-    light_intensity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(3)])
-    temperature = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(2)])
-    location_type = models.CharField(max_length=50)
-    water_need = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(4)])
-    growth = models.IntegerField()
+    light_intensity = models.PositiveIntegerField(default=1, choices=LIGHT_CHOICES)
+    temperature = models.PositiveIntegerField(default=1, choices=TEMPERATURE_CHOICES)
+    location_type = models.PositiveIntegerField(default=1, choices=LOCATION_TYPE_CHOICES)
+    water = models.PositiveIntegerField(default=1, choices=WATER_CHOICES)
+    growth = models.PositiveIntegerField(default=1, choices=GROWTH_CHOICES)
     pet_compatible = models.BooleanField()
     allergy_compatible = models.BooleanField()
-    attention_need = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(3)])
+    attention_need = models.PositiveIntegerField(default=1, choices=ATTENTION_NEED_CHOICES)
     edible = models.BooleanField()
     wikipedia_link = models.URLField()
-    special_condition = models.TextField()
-    main_image = models.ImageField()
-    season = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(4)])
+    special_condition = models.TextField(null=True, blank=True)
+    season = models.PositiveIntegerField(default=1, choices=SEASON_CHOICES)
     is_seasonal = models.BooleanField(default=False)
+    main_img = models.ImageField(upload_to='/plants/Main_images/')
 
     def __str__(self):
         return self.name
+
+
+class PlantImage(models.Model):
+    img = models.ImageField(upload_to='/plants/images/')
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
