c95efbcd21e4b50b68b53cd97c00bfc1ed7c81c2
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-26 20:25:57 +0330

    Compelting the explore api

--- home/views.py
+++ home/views.py
@@ -1,14 +1,26 @@
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import permissions, status
-from plants.models import Plants
-from plants.serializers import PlantsSerializer
+from plants.models import Plant
+from plants.serializers import PlantSerializer
 
 
 class ExploreAPI(ListAPIView):
-    serializer_class = PlantsSerializer
+    serializer_class = PlantSerializer
     permission_classes = [AllowAny]
-    queryset = Plants.objects.all()
+    # It can be safely remove if we only use get_queryset function
+    queryset = Plant.objects.filter(is_valid=True)
+    lookup_url_kwarg = ['type', 'light_intensity', 'temperature', 'location_type', 'water', 'growth',
+                        'attention_need', 'season', 'is_seasonal', 'fragrance', 'pet_compatible',
+                        'allergy_compatible', 'edible', ]
+
+    def get_queryset(self):
+        queryset = Plant.objects.filter(is_valid=True)
+        filter = {}
+        for field in self.lookup_url_kwarg:
+            if self.request.query_params.get(field):
+                filter[field] = self.request.query_params[field]
+        return Plant.objects.filter(**filter)
 
