0ea9259f6d019026261eac84eef159c0843d39ec
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-29 12:01:52 +0330

    Adding the garden model

---
+++ garden/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Garden
+
+
+@admin.register(Garden)
+class GardenAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'business_code', 'avg_score',)
