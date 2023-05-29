eca91b7d4734625c9dd78d49d057e5100c459bde
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-29 13:29:25 +0330

    Rebasing the branch

--- garden/views.py
+++ garden/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import permissions
+from .models import Garden
+from .serializers import GardenSerializer
 
-# Create your views here.
+
+class GardenAPI(RetrieveAPIView):
+    serializer_class = GardenSerializer
+    permission_classes = [AllowAny]
+    queryset = Garden.objects.filter(is_verified=True)
+
+
