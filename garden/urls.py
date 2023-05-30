d3e6b217d51ad42684f3d47a5239c18f949de45b
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-30 15:01:57 +0330

    Adding the url and api of garden update

--- garden/urls.py
+++ garden/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'garden'#todo what is this
 urlpatterns = [
-    path('<int:id>', views.GardenAPI.as_view(), name='explore_api'),
+    path('<int:id>', views.GardenAPI.as_view(), name='get_garden_api'),
+    path('<int:id>/update', views.GardenUpdateAPI.as_view(), name='update_garden_api'),
 ]
