a6e0cd1f9de1aa7f8fe388c2ed88f95dc5b17915
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-30 17:13:56 +0330

    Adding the delete and create api view

--- garden/views.py
+++ garden/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, CreateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import permissions
 from .models import Garden
-from .serializers import GardenSerializer, GardenUpdateSerializer
+from .serializers import GardenSerializer, GardenUpdateSerializer, GardenCreateSerializer
 
 
 class GardenAPI(RetrieveAPIView):
@@ -16,4 +16,23 @@ class GardenUpdateAPI(UpdateAPIView):
     serializer_class = GardenUpdateSerializer
     permission_classes = [AllowAny] #Todo Change it to IsAuthenticated
     queryset = Garden.objects.filter(is_verified=True)
+    lookup_field = 'id'
+
+
+class GardenCreateAPI(CreateAPIView):
+    serializer_class = GardenCreateSerializer
+    permission_classes = [AllowAny]  # Todo Change it to IsAuthenticated
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['garden_owner'] = request.user.email
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+
+class GardenDeleteAPI(DestroyAPIView):
+    serializer_class = GardenSerializer
+    permission_classes = [AllowAny]
+    queryset = Garden.objects.all()
     lookup_field = 'id'
