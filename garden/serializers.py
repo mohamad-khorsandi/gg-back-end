e5a4d0b193553f049766b33f72dc362306af9113
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-31 13:52:38 +0330

    writing some new serializers for better usability

--- garden/serializers.py
+++ garden/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Garden
+from accounts.serializers import GardenOwnerSerializer
+from plants.serializers import PlantSerializer
 
 
 class GardenSerializer(serializers.ModelSerializer):
+    plants = PlantSerializer(many=True)
+
     class Meta:
         model = Garden
         fields = '__all__'
@@ -12,3 +16,11 @@ class GardenUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Garden
         exclude = ['garden_owner', 'is_verified', 'business_code',]
+
+
+class GardenCreateSerializer(serializers.ModelSerializer):
+   # garden_owner = GardenOwnerSerializer()
+
+    class Meta:
+        model = Garden
+        exclude = ['is_verified', 'business_code',]
