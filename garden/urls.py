db9d250dff7cc52477d65a12ec10bf078ae67de5
SMS-Sajadi <soroush.s2017@gmail.com>
2023-06-02 01:40:40 +0330

    Adding urls

--- garden/urls.py
+++ garden/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('create/', views.GardenCreateAPI.as_view(), name='create_garden'),
     path('delete/', views.GardenDeleteAPI.as_view(), name='delete_garden'),
     path('<int:id>/add_score/', views.GardenAddScoreAPI.as_view(), name='add_score'),
+    path('<int:id>/update_score/', views.GardenScoreUpdateAPI.as_view(), name='update_score'),
+    path('<int:id>/delete_score/', views.GardenScoreDeleteAPI.as_view(), name='delete_score'),
 ]
