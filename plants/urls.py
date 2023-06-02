b8855022e7d59ff795357a6e84b84a773d23fd93
SMS-Sajadi <soroush.s2017@gmail.com>
2023-06-02 18:21:44 +0330

    removing todo

--- plants/urls.py
+++ plants/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'plants'#todo what is this
+app_name = 'plants'
 urlpatterns = [
     path('filter/', views.PlantFilter.as_view(), name='explore_api'),
     path('<int:id>', views.PlantDetails.as_view(), name='plant_details'),
