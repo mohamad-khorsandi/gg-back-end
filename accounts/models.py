4e5a06a56c203ae61fe71eaae965618ba29e8977
mohammad <9m.kh557@gmai.com>
2023-06-02 22:13:49 +0330

    add plant list

--- accounts/models.py
+++ accounts/models.py
@@ -108,7 +108,7 @@ class TemporaryUser(models.Model):
     created = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=128)
-    phone_number = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
     is_garden_owner = models.BooleanField(default=False)
 
     def __str__(self):
